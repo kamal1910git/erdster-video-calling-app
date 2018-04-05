188ddafc892426b352c603c7ead4819779a990a3
Kamalakannan Ayyau <kamalakannan.ayyau@brillio.com>
2018-04-05 11:27:14 +0530

    Commit all again

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   plugins:[
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('development')
+        'NODE_ENV': JSON.stringify('production')
       }
     })
   ],
