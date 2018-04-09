fb95e697f439c4234c26c764e4627f507211c9e0
kamal <kamalakannan.ayyau@brillio.com>
2018-04-09 14:50:18 +0530

    cors fixes

--- server.babel.js
+++ server.babel.js
@@ -7,6 +7,7 @@ import sio from 'socket.io';
 import favicon from 'serve-favicon';
 import compression from 'compression';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import s3Router from './s3Router';
 
 const app = express(),
@@ -24,9 +25,34 @@ app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
-  console.log(req.path);
-  next();
+  var oneof = false;
+    if(req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if(oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
+
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
 });
+
+app.use(cors());
+app.options('*', cors());
 
 app.use(bodyParser.json());
 
