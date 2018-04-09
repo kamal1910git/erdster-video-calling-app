4dbf6cb6483c00c4612c05f07231a12c3790cb44
kamal <kamalakannan.ayyau@brillio.com>
2018-04-09 15:01:46 +0530

    cors fixes

--- server.babel.js
+++ server.babel.js
@@ -25,30 +25,28 @@ app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
-  var oneof = false;
-    if(req.headers.origin) {
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
-        oneof = true;
-    }
-    if(req.headers['access-control-request-method']) {
-        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-        oneof = true;
-    }
-    if(req.headers['access-control-request-headers']) {
-        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-        oneof = true;
-    }
-    if(oneof) {
-        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-    }
+  var responseSettings = {
+    "AccessControlAllowOrigin": req.headers.origin,
+    "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+    "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
+    "AccessControlAllowCredentials": true
+};
 
-    // intercept OPTIONS method
-    if (oneof && req.method == 'OPTIONS') {
-        res.send(200);
-    }
-    else {
-        next();
-    }
+/**
+ * Headers
+ */
+res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
+res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+
+if ('OPTIONS' == req.method) {
+    res.send(200);
+}
+else {
+    next();
+}
+
 });
 
 app.use(cors());
