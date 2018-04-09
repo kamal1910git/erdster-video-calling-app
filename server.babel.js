c1e2c7bedfd5b5998161c1dc75d99d2e75ae4833
kamal <kamalakannan.ayyau@brillio.com>
2018-04-09 15:15:59 +0530

    cors fixes

--- server.babel.js
+++ server.babel.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   var responseSettings = {
-    "AccessControlAllowOrigin": req.headers.origin,
+    "AccessControlAllowOrigin": "https://videocallingapp.herokuapp.com",
     "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
     "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
     "AccessControlAllowCredentials": true
