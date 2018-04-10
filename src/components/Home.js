5a97d2864ddf2206b896aa0afcadc3243b96d77e
Kamalakannan Ayyau <kamalakannan.ayyau@brillio.com>
2018-04-10 15:52:00 +0530

    online-offline checking

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router'
+import { Offline, Online } from 'react-detect-offline';
 
 const Home = props =>
   <div className="home">
     <div>
+    <Offline><p>You are offline...</p></Offline>
       <h1 itemProp="headline">Erdster :: Video Calling :: Room Creation</h1>
       <p>Please enter a room name.</p>
       <form onSubmit={props.joinRoom}>
         <input type="text" name="room" value={props.roomId} onChange={props.handleChange} pattern="^\w+$" maxLength="10" required autoFocus title="Room name should only contain letters or numbers."/>
-        <button className="primary-button" type="submit">Join</button>
+        <Online><button className="primary-button" type="submit">Join</button></Online>        
       </form>      
     </div>
   </div>;
