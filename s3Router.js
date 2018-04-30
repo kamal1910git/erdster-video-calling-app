7e345571ad4c5a86325449e9bf36cd2ceba8a03f
kamal <kamalakannan.ayyau@brillio.com>
2018-04-30 13:26:09 +0530

    Recording save with conditional

--- s3Router.js
+++ s3Router.js
@@ -32,8 +32,8 @@ function S3Router(options) {
     aws.config.accessKeyId = process.env.AWS_ACCESS_KEY_ID;
     aws.config.secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY; 
     aws.config.region = process.env.AWS_REGION;
-    aws.config.signatureVersion = 'V4';
-    
+    aws.config.signatureVersion = 'v4';
+
     var s3 = new aws.S3();
 
     var params = {
