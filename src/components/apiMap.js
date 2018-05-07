329623697ccdefdb5580f8a8591fba642b670e1e
kamal <kamalakannan.ayyau@brillio.com>
2018-05-07 10:29:53 +0530

    Roomlist mongodb integration

---
+++ src/components/apiMap.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BASE_API = "/api/v0";
+
+ const API_CONSTANT_MAP = {
+  "saveroomlist": `${BASE_API}/SaveRoomList/`,
+  "updateroomlist": `${BASE_API}/UpdateRoomlist/`,
+  "removeroomlist": `${BASE_API}/RemoveRoomlist/`,
+  "getroomlist": `${BASE_API}/GetRoomlist/`,
+  "getroomlistbyname": `${BASE_API}/GetRoomlistByName/`,
+  "sendemail": `${BASE_API}/sendemail/`
+};
+
+export default API_CONSTANT_MAP;
