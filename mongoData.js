db0687a9ebe62b8f085aaf69e15a849aa12c73b7
kamal <kamalakannan.ayyau@brillio.com>
2018-05-14 18:45:33 +0530

    Room fixes

--- mongoData.js
+++ mongoData.js
@@ -4,7 +4,7 @@ var ObjectId = require('mongodb').ObjectID;
 module.exports = function(app) {
 
 var mongo = require("mongoose");  
-var db = mongo.connect("mongodb://mongodbuser:welcome123@ds215910.mlab.com:15910/videocalling", function(err, response){   
+var db = mongo.connect(process.env.MONGODB_SERVER, function(err, response){    
    if(err)
    { console.log('Failed to connect to ' + db); }  
    else
